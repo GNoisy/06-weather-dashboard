e9089823b348b1f29b0aac39f009f4a23a020cd8
Giovanni Noisy <gnoisy77@gmail.com>
2020-06-28 10:34:26 -0700

    simplified js code to push city data to html

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,54 @@
 $(document).ready(function () {
 
-    jQuery.ajaxPrefilter(function (options) {
-        if (options.crossDomain && jQuery.support.cors) {
-          options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-        }
-    });
-
-
-    $("#add-cty").on('submit', function (event) {
-        event.preventDefault();
+    console.log("hello");
+    // var cityInput = $("#city-name").val();
+    var cityInput = "Oakland";
+    var APIKey = "21cb192c9ac239ad019084d15bf75466";
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=" + APIKey;
     
-        var cityInput = $("#city-name").val();
-        var APIKey = "21cb192c9ac239ad019084d15bf75466";
-        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=" + APIKey;
 
-        // ajax call for city input by user
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-          }).then(function(response) {
+    // $("#add-city").on("click", function (event) {
+        
+    
+    //     // ajax call for city input by user
+    //     $.ajax({
+    //         url: queryURL,
+    //         method: "GET"
+    //       }).then(function(response) {
             
   
-            console.log(response);
-            // Storing the rating data
-            var rating = response.Rated;
+    //         console.log(response);
+    //         // Storing the rating data
+    //         var rating = response.Rated;
   
             
-          });
+    //     });
 
         
-    })
+    // })
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        
+
+        console.log(response);
+        var iconcode = response.list[0].weather[0].icon;
+        var iconURL = "https://openweathermap.org/img/wn/" + iconcode + "@2x.png";
+        
+        
+
+        $("#main-data").append("<h2>" + response.city.name + "</h2>");
+        $("#main-data").append("<div> <p>Temperature: " + response.list[0].main.temp + "&deg;F</p> </div>");
+
+        
+    });
+
+    
+    
+
+    
 
         
 
