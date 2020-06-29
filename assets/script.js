e61819cc0b99dd6e52f02c6b99fe61bb1557ce30
Giovanni Noisy <gnoisy77@gmail.com>
2020-06-28 22:10:30 -0700

    added css styling to forecast divs

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
 
+    jQuery.ajaxPrefilter(function (options) {
+        if (options.crossDomain && jQuery.support.cors) {
+          options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+        }
+    });
+
     // $("#currentDay").text(moment().format("dddd, MMMM Do"));
     $("#currentDay").text(moment().format('DD/MM/YYYY'));
     // moment().calendar(referenceDay);
@@ -11,10 +17,63 @@ $(document).ready(function () {
     var APIKey = "21cb192c9ac239ad019084d15bf75466";
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&units=imperial&appid=" + APIKey;
     
+    
     //uv index api key
     var uvIndexAPIKey = "68a5bd8b2cb505f15ab45ed0f4e63629";
 
-    // $("#add-city").on("click", function (event) {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        
+
+        console.log(response);
+        //var to target icon number in array
+        var iconcode = response.list[0].weather[0].icon;
+        // var iconURL = "https://openweathermap.org/img/wn/" + iconcode + "@2x.png";
+        var iconImage = "<img src='https://openweathermap.org/img/wn/" + iconcode + ".png'>"
+        // <img src='https://openweathermap.org/img/wn/" + iconcode + ".png'>"
+
+        $("#main-data").append("<h1>" + response.city.name + iconImage + "</h1>");
+        // $("#main-data").append("<p>Current Conditions: " + iconImage + "</p>");
+        $("#main-data").append("<div> <p><strong>Current Condition</strong>: " + response.list[0].weather[0].description + "</p> </div>");
+        $("#main-data").append("<div> <p><strong>Temperature</strong>: " + response.list[0].main.temp + " &deg;F</p> </div>");
+        $("#main-data").append("<div> <p><strong>Humdity</strong>: " + response.list[0].main.humidity + "%</p> </div>");
+        $("#main-data").append("<div> <p><strong>Wind Speed</strong>: " + response.list[0].wind.speed + "</p> </div>")
+        $("#main-data").append("<div> <p><strong>UV Index</strong>: " + "</p></div>")
+
+        
+    });
+
+    // //uv index api key
+    // // var uvIndexAPIKey = "68a5bd8b2cb505f15ab45ed0f4e63629";
+    // var query2URL = "http://api.openweathermap.org/data/2.5/uvi?appid=68a5bd8b2cb505f15ab45ed0f4e63629&lat=" + response.city.coord.lat + "&lon=" + response.city.coord.lon + "";
+
+    // $.ajax({
+    //     url: query2URL,
+    //     method: "GET"
+    //   }).then(function(response2) {
+        
+
+    //     console.log(response2);
+        
+        
+    // });
+
+    
+    
+
+    
+ 
+        
+
+    
+});
+
+
+
+
+ // $("#add-city").on("click", function (event) {
         
     
     //     // ajax call for city input by user
@@ -33,38 +92,3 @@ $(document).ready(function () {
 
         
     // })
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-        
-
-        console.log(response);
-        //var to target icon number in array
-        var iconcode = response.list[0].weather[0].icon;
-        // var iconURL = "https://openweathermap.org/img/wn/" + iconcode + "@2x.png";
-        var iconImage = "<img src='https://openweathermap.org/img/wn/" + iconcode + ".png'>"
-        // <img src='https://openweathermap.org/img/wn/" + iconcode + ".png'>"
-
-        $("#main-data").append("<h1>" + response.city.name + "</h1>");
-        // $("#main-data").append("<p>Current Conditions: " + iconImage + "</p>");
-        $("#main-data").append("<div> <p><strong>Current Condition</strong>: " + response.list[0].weather[0].description + "</p> </div>");
-        $("#main-data").append("<div> <p><strong>Temperature</strong>: " + response.list[0].main.temp + " &deg;F</p> </div>");
-        $("#main-data").append("<div> <p><strong>Humdity</strong>: " + response.list[0].main.humidity + "%</p> </div>");
-        $("#main-data").append("<div> <p><strong>Wind Speed</strong>: " + response.list[0].wind.speed + "</p> </div>")
-
-        
-    });
-
-    
-    
-
-    
- 
-        
-
-    
-});
-
-
