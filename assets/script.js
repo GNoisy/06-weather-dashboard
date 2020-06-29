28759f332685424ad97fe02734fadd3a11254d92
Giovanni Noisy <gnoisy77@gmail.com>
2020-06-28 22:57:51 -0700

    added content to 5 day forecast divs and styled those divs in css

--- assets/script.js
+++ assets/script.js
@@ -18,8 +18,6 @@ $(document).ready(function () {
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&units=imperial&appid=" + APIKey;
     
     
-    //uv index api key
-    var uvIndexAPIKey = "68a5bd8b2cb505f15ab45ed0f4e63629";
 
     $.ajax({
         url: queryURL,
@@ -34,14 +32,38 @@ $(document).ready(function () {
         var iconImage = "<img src='https://openweathermap.org/img/wn/" + iconcode + ".png'>"
         // <img src='https://openweathermap.org/img/wn/" + iconcode + ".png'>"
 
+        //main section of html page
         $("#main-data").append("<h1>" + response.city.name + iconImage + "</h1>");
         // $("#main-data").append("<p>Current Conditions: " + iconImage + "</p>");
         $("#main-data").append("<div> <p><strong>Current Condition</strong>: " + response.list[0].weather[0].description + "</p> </div>");
         $("#main-data").append("<div> <p><strong>Temperature</strong>: " + response.list[0].main.temp + " &deg;F</p> </div>");
         $("#main-data").append("<div> <p><strong>Humdity</strong>: " + response.list[0].main.humidity + "%</p> </div>");
-        $("#main-data").append("<div> <p><strong>Wind Speed</strong>: " + response.list[0].wind.speed + "</p> </div>")
-        $("#main-data").append("<div> <p><strong>UV Index</strong>: " + "</p></div>")
+        $("#main-data").append("<div> <p><strong>Wind Speed</strong>: " + response.list[0].wind.speed + "</p> </div>");
+        $("#main-data").append("<div> <p><strong>UV Index</strong>: " + "</p></div>");
 
+        
+        //five day forecast divs
+        //div 1
+        $("#fcast1").append("<p>Temp: " + response.list[1].main.temp + " &deg;F</p>");
+        $("#fcast1").append("<p>Humdity: " + response.list[1].main.humidity + "%</p>");
+
+        //div2
+        $("#fcast2").append("<p>Temp: " + response.list[2].main.temp + " &deg;F</p>");
+        $("#fcast2").append("<p>Humdity: " + response.list[2].main.humidity + "%</p>");
+
+        //div3
+        $("#fcast3").append("<p>Temp: " + response.list[3].main.temp + " &deg;F</p>");
+        $("#fcast3").append("<p>Humdity: " + response.list[3].main.humidity + "%</p>");
+
+        //div4
+        $("#fcast4").append("<p>Temp: " + response.list[4].main.temp + " &deg;F</p>");
+        $("#fcast4").append("<p>Humdity: " + response.list[4].main.humidity + "%</p>");
+
+        //div4
+        $("#fcast5").append("<p>Temp: " + response.list[5].main.temp + " &deg;F</p>");
+        $("#fcast5").append("<p>Humdity: " + response.list[5].main.humidity + "%</p>");
+    
+        
         
     });
 
